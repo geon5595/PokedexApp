ce9915df5a87e6977f47f3fd04b9879e430b32f6
geon5595 <geon5595@naver.com>
2024-08-01 17:36:26 +0900

    3단계: MainViewModel 추가

--- PokedexApp/ViewModels/MainViewModel.swift
+++ PokedexApp/ViewModels/MainViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import RxSwift
+
+class MainViewModel {
+  private let disposeBag = DisposeBag()
+  
+  let pokemonSubject = BehaviorSubject(value: [Pokemon]())
+  private var limit = 20
+  private var offset = 0
+  init() {
+    
+  }
+  
+  func fetchPokemonData() {
+    guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=\(limit)&offset=\(offset)") else {
+      pokemonSubject.onError(NetworkError.invalidUrl)
+      return
+    }
+    NetworkManager.shared.fetch(url: url)
+      .subscribe(onSuccess: { [weak self] (pokemonresponse: PokemonResponse) in
+        self?.pokemonSubject.onNext(pokemonresponse.results)
+      }, onFailure:  { [weak self] error in
+        self?.pokemonSubject.onError(NetworkError.decodingFail)
+      }).disposed(by: disposeBag)
+  }
+}
